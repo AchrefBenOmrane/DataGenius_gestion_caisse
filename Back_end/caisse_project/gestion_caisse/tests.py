7e1ec97324c149d6730cfa919cd6a2808d24fb6e
achrefbo98 <46647953+achrefbo98@users.noreply.github.com>
2020-12-14 18:32:21 +0100

    ajout des commentaires

--- Back_end/caisse_project/gestion_caisse/tests.py
+++ Back_end/caisse_project/gestion_caisse/tests.py
@@ -4,14 +4,15 @@ from gestion_caisse.models import caisse
 #Test unitaire sur le prix et la quantité des produit pour le model caisse 
 
 class Caisse(TestCase):
-    """ Test module for caisse model """
 
+#Fonction pour configurer les objets
     def setUp(self):
         caisse.objects.create(
             name='banane', price=3, quantity=5)
         caisse.objects.create(
             name='pomme', price=1, quantity=6)
-
+        
+#Fonction pour tester les prix des produits 
     def test_prix(self):
         caisse_banane = caisse.objects.get(name='banane')
         caisse_apple = caisse.objects.get(name='pomme')
@@ -19,7 +20,8 @@ class Caisse(TestCase):
             caisse_banane.get_prix(), "le prix de banane est 3 €")
         self.assertEqual(
             caisse_apple.get_prix(), "le prix de pomme est 1 €")
-        
+
+#Fonction pour tester les Quantitées des produits 
     def test_quantity(self):
         Q_banane = caisse.objects.get(name='banane')
         Q_apple = caisse.objects.get(name='pomme')
